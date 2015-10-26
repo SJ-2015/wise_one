e9e284caf54a16383a258115ac24e2d84c0d60a1
Sharon Ji <sharon.bowei.ji@gmail.com>
2015-10-26 13:55:19 -0700

    decisionCRUD completed

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -2,21 +2,52 @@ class DecisionsController < ApplicationController
 
   def create
 
-    @decision = Decision.new
+    #@decision = Decision.new
     decision_params = params.require(:decision).permit(:name, :description)
     decision_params[:user_id] = @current_user.id
-
     @decision = Decision.create(decision_params)
-
     redirect_to user_path(current_user)
-    
+
   end		
 
   def destroy
     id = params[:id]
+    
     @decision = Decision.find(id)
-    @decison.destroy
-    redirect_to decisions_path
+    @decision.destroy
+
+    redirect_to user_path(current_user)
+
   end 
+
+  def show
+    id = params[:id]
+    @decision = Decision.find(id)
+    
+    #@options= @decision.options
+
+  end
+
+  def edit
+    id = params[:id]
+    @decision = Decision.find(params[:id])
+  end
+
+  def update
+    decision_id = params[:id]
+    @decision = Decision.find(decision_id)
+    @user = current_user
+
+    #get updated data
+    updated_attributes = params.require(:decision).permit(:name, :descripton)
+
+    if @decision.update_attributes(updated_attributes)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end  
+  end
+
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # before_action :new_decision, only: [:show, :index]
+
 	# grab the users
   def index
     @users = User.all
@@ -22,8 +24,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    
-    @decision = @user.decisions.new
+    @decision = Decision.new
     render :show
 
   end
